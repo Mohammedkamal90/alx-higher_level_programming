5fe1a8cb377e0b3473b683ffdf51f867485af102
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-08-16 18:20:08 -0700

    all

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """replace / add key/value pair at dictionary"""
+    a_dictionary[key] = value
+    return (a_dictionary)
