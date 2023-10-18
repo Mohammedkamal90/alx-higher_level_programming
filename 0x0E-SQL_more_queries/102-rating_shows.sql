f99448e51889faf3a4a14a8d20ad03012d95c31c
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-10-18 05:51:52 -0700

    rating

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- List all show from hbtn_0d_tvshows_rate by rating
+-- List all rows of table by sum of row  linked
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
