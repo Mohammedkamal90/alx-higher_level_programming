31d86fc9e99d92ce037c057b3f3ad13fe8d7529c
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-11-14 03:49:08 -0800

    task4

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Create empty object if w or h is equal to 0 or not positive integer
+      Object.create(null);
+    }
+  }
+
+  print() {
+    // Print rectangle use character X
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
