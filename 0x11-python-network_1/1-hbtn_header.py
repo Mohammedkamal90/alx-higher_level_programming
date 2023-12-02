17467b631197e5858e9d8357905c934a6238b090
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-12-02 11:13:53 -0800

    task1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
+"""Take URL send display value X-Request-Id"""
 import urllib.request
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-
-    with urllib.request.urlopen(url) as response:
-        x_request_id = response.getheader('https://alx-intranet.hbtn.io/status')
-        print(x_request_id)
-        print("\t- type: {}".format(type(x_request_id)))
-        print("\t- content: {}".format(x_request_id))
-        print("\t- utf8 content: {}".format(x_request_id.decode("utf-8")))
+if len(argv) > 1:
+    with urllib.request.urlopen(argv[1]) as response:
+        print(response.getheader("X-Request-Id"))
