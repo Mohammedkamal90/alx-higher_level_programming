dd99ab7c4f7d6d5335874e4526f79bad8ba0297b
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-12-02 11:20:30 -0800

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,9 +3,9 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    repo_name = sys.argv[1]
-    owner_name = sys.argv[2]
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    repo = argv[1]
+    owner = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(repo, owner)
     params = {'per_page': 10}
 
     response = requests.get(url, params=params)
@@ -17,4 +17,4 @@ if __name__ == "__main__":
             author_name = commit.get('commit').get('author').get('name')
             print(f"{sha}: {author_name}")
     except ValueError:
-        print("Error: Unable to fetch commits.")
+        print("Error: Unable to fetch commits")
