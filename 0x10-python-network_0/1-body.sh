3c69d719a7e7c41a30fc81597632303e0150b096
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-12-02 10:39:29 -0800

    1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
-# Check if the URL is provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Use curl to send a GET request and display the body of the response for a 200 status code
-curl -s -o /dev/null -w "%{http_code}" "$1" | {
-  read -r status_code
-  if [ "$status_code" -eq 200 ]; then
-    curl -s "$1"
-  fi
-}
+# Check if url Use curl to send a GET request 
+curl -sL "$1"
