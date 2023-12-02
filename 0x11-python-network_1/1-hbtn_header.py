adb2e9e1e006b9e5d8ca1c59b5c068e33d07b61c
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-12-02 10:29:21 -0800

    update

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,5 +6,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
-        x_request_id = response.getheader('X-Request-Id')
+        x_request_id = response.getheader('https://alx-intranet.hbtn.io/status')
         print(x_request_id)
+        print("\t- type: {}".format(type(x_request_id)))
+        print("\t- content: {}".format(x_request_id))
+        print("\t- utf8 content: {}".format(x_request_id.decode("utf-8")))
