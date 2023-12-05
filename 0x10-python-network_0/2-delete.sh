116c3ea250d2a5b844d4aa83068b67e4d38c233d
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-12-05 15:16:52 -0800

    del

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Check if the URL is provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
 # Use curl to send a DELETE request and display the body of the response
-curl -X DELETE -s "$1"
+curl -sX DELETE "$1"
