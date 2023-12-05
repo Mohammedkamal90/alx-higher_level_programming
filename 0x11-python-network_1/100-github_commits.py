1d80388643664bd7ec0f1e011625693edace69d7
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-12-05 15:42:25 -0800

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,15 +3,27 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+    if len(sys.argv) != 3:
+        print("Usage: {} <repository> <owner>".format(sys.argv[0]))
+        sys.exit(1)
 
-    r = requests.get(url)
-    commits = r.json()
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
+
     try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error:", errh)
+        sys.exit(1)
+    except requests.exceptions.RequestException as err:
+        print("Request Error:", err)
+        sys.exit(1)
+
+    commits = response.json()[:10]
+
+    for commit in commits:
+        sha = commit.get('sha')
+        author_name = commit.get('commit', {}).get('author', {}).get('name')
+        print(f"{sha}: {author_name}")
