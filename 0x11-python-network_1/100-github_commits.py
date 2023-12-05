7105609c1cf85b0967a22a793d9eea7e825126c1
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-12-05 15:38:25 -0800

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,27 +3,15 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: {} <repository> <owner>".format(sys.argv[0]))
-        sys.exit(1)
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    repository = sys.argv[1]
-    owner = sys.argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repository)
-
+    r = requests.get(url)
+    commits = r.json()
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as errh:
-        print("HTTP Error:", errh)
-        sys.exit(1)
-    except requests.exceptions.RequestException as err:
-        print("Request Error:", err)
-        sys.exit(1)
-
-    commits = response.json()[:10]
-
-    for commit in commits:
-        sha = commit.get('sha')
-        author_name = commit.get('commit', {}).get('author', {}).get('name')
-        print("{}: {}".format(sha, author_name))
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
