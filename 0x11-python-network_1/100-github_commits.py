4118a38c96878ee3d762a59162be2af9f00a52b1
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-12-05 15:06:19 -0800

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,18 +3,27 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    repo = argv[1]
-    owner = argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(repo, owner)
-    params = {'per_page': 10}
+    if len(sys.argv) != 3:
+        print("Usage: {} <repository> <owner>".format(sys.argv[0]))
+        sys.exit(1)
 
-    response = requests.get(url, params=params)
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repository)
 
     try:
-        commits = response.json()
-        for commit in commits:
-            sha = commit.get('sha')
-            author_name = commit.get('commit').get('author').get('name')
-            print(f"{sha}: {author_name}")
-    except ValueError:
-        print("Error: Unable to fetch commits")
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error:", errh)
+        sys.exit(1)
+    except requests.exceptions.RequestException as err:
+        print("Request Error:", err)
+        sys.exit(1)
+
+    commits = response.json()[:10]
+
+    for commit in commits:
+        sha = commit.get('sha')
+        author_name = commit.get('commit', {}).get('author', {}).get('name')
+        print("{}: {}".format(sha, author_name))
