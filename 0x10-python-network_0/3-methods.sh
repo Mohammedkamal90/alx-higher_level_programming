b7728f0952aa90d57d2b19775c65a780814e3388
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-12-05 15:21:14 -0800

    upd

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use curl to send an OPTIONS request and display the allowed HTTP methods
-curl -sI -X OPTIONS "$1" | grep -i Allow | awk '{print $2}'
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if the URL is provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
 # Use curl to send a GET request with a custom header and display the body of the response
-curl -s -H "X-School-User-Id: 98" "$1"
+curl -sH "X-School-User-Id: 98" "$1"
