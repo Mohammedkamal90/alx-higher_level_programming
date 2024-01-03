ca4c6052b5a11cc3b8ea6573bb1d9a0b64a5bb29
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-01-03 14:23:23 -0800

    task5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,5 +1,21 @@
-
 #!/usr/bin/node
+
 const fs = require('fs');
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+
+const [, , url, filePath] = process.argv;
+
+const fileStream = fs.createWriteStream(filePath);
+
+fileStream.on('error', (err) => {
+  console.error(`Error writing to file: ${err}`);
+});
+
+request(url)
+  .on('error', (err) => {
+    console.error(`Error making request: ${err}`);
+  })
+  .pipe(fileStream)
+  .on('finish', () => {
+    console.log(`Content from ${url} has been stored in ${filePath}`);
+  });
